e44fe612401839af8a4b8e2dafaaa03f64c62c34
guolanting <guolanting@haplox.com>
2015-12-25 00:12:27 +0000

    add a test for query_gene

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,10 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+let gene = "KRAS", genesyn = ["KRAS","C-K-RAS","CFC2","K-RAS2A","K-RAS2B",
+                              "K-RAS4A","K-RAS4B","KI-RAS","KRAS1","KRAS2",
+                              "NS","NS3","RALD","RASK2"]
+    @test query_gene("KRAS") == Set(genesyn)
+
+end
