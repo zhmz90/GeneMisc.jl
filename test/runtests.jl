b1bfe2363d44c59f42938ee882bb52e03bb1e158
guolanting <guolanting@haplox.com>
2015-12-25 07:59:30 +0000

    add a test for query_gene given its location

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,11 @@ using Base.Test
 let gene = "KRAS", genesyn = ["KRAS","C-K-RAS","CFC2","K-RAS2A","K-RAS2B",
                               "K-RAS4A","K-RAS4B","KI-RAS","KRAS1","KRAS2",
                               "NS","NS3","RALD","RASK2"]
+    
     @test query_gene("KRAS") == Set(genesyn)
 
+end
+
+let chr="chr20",pos="45314173"
+    @test query_gene(chr,pos) == "TP53RK"
 end
