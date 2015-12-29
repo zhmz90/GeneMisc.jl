4da5b98b18d380304923d1747a167a71f99455b1
guolanting <guolanting@haplox.com>
2015-12-29 03:25:28 +0000

    fix a file name  typo

--- src/index/index.jl
+++ src/index/index.jl
@@ -18,14 +18,15 @@ end
 @doc """ load index for synonym genes
 """ ->
 function load_index()
-    if isfile(id_genes_fl) || isfile(gene_id_fl)  || isfile(chrsted_gene_fl)
+    if isfile(id_genes_fl) || isfile(gene_id_fl)  ||
+        isfile(chr_sted_gene_fl) || isfile(gene_chrsted_fl)
         build_index()
     end
     
     global id_genes = load(id_genes_fl, "id_genes_dict")
     global gene_id  = load(gene_id_fl,  "gene_id_dict")
     global chr_sted_gene = load(chr_sted_gene_fl, "chr_sted_gene_dict")
-    global gene_chrsted  = load(gene_chrsted, "gene_chrsted_dict")
+    global gene_chrsted  = load(gene_chrsted_fl, "gene_chrsted_dict")
     gene_id,id_genes,chr_sted_gene,gene_chrsted
 end
 
