8a71dac1d3e520f09c15f37ac71dc102d15d66fc
guolanting <guolanting@haplox.com>
2015-12-29 02:44:56 +0000

    load index gene_chrsted

--- src/index/index.jl
+++ src/index/index.jl
@@ -24,8 +24,9 @@ function load_index()
     
     global id_genes = load(id_genes_fl, "id_genes_dict")
     global gene_id  = load(gene_id_fl,  "gene_id_dict")
-    global chrsted_gene = load(chrsted_gene_fl, "chrsted_gene_dict")
-    gene_id,id_genes,chrsted_gene
+    global chr_sted_gene = load(chr_sted_gene_fl, "chr_sted_gene_dict")
+    global gene_chrsted  = load(gene_chrsted, "gene_chrsted_dict")
+    gene_id,id_genes,chr_sted_gene,gene_chrsted
 end
 
 
